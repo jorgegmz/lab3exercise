eace7854791fbc6eced99f50734f0492ca4113f4
jag028 <jag.gmz028@gmail.com>
2016-04-18 16:57:57 -0700

    sum function works and testers done

--- app/src/androidTest/java/tests/JUnit_test.java
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -1,8 +1,13 @@
 package tests;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.jag028.lab3exercise.MainActivity;
+import com.example.jag028.lab3exercise.R;
 
 /**
  * Created by jag028 on 4/15/2016.
@@ -13,7 +18,41 @@ public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
         super(MainActivity.class);
     }
 
+    @UiThreadTest
     public void test_sum(){
         mainActivity = getActivity();
+
+        EditText et1 = (EditText) mainActivity.findViewById(R.id.TFnumber1);
+        EditText et2 = (EditText) mainActivity.findViewById(R.id.TFnumber2);
+        TextView rst = (TextView) mainActivity.findViewById(R.id.TVresult);
+        Button bt = (Button) mainActivity.findViewById(R.id.button);
+
+        //testing positive #'s
+        et1.setText("5");
+        et2.setText("8");
+        bt.performClick();
+        int result = Integer.parseInt(rst.getText().toString());
+        assertEquals(result > 0, true);
+
+        //testing negative #'s
+        et1.setText("-6");
+        et2.setText("-9");
+        bt.performClick();
+        result = Integer.parseInt(rst.getText().toString());
+        assertEquals(result < 0, true);
+
+        //testing pos and neg #'s
+        et1.setText("5");
+        et2.setText("-8");
+        bt.performClick();
+        result = Integer.parseInt(rst.getText().toString());
+        assertEquals(result == -3, true);
+
+        //testing neg pos #'s
+        et1.setText("-5");
+        et2.setText("8");
+        bt.performClick();
+        result = Integer.parseInt(rst.getText().toString());
+        assertEquals(result == 3, true);
     }
 }
--- app/src/main/java/com/example/jag028/lab3exercise/MainActivity.java
+++ app/src/main/java/com/example/jag028/lab3exercise/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.test.UiThreadTest;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,59 +14,39 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @UiThreadTest
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
+    @UiThreadTest
+    public int sum(int x, int y){
+        return x+y;
+    }
+
+    @UiThreadTest
     public void onAddClick(View a){
         if(a.getId() == R.id.button){
             EditText a1 = (EditText)findViewById(R.id.TFnumber1);
             EditText a2 = (EditText)findViewById(R.id.TFnumber2);
 
-            int num1, num2, result;
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
 
             num1 = Integer.parseInt(a1.getText().toString());
             num2 = Integer.parseInt(a2.getText().toString());
 
-            result = num1 + num2;
+            result = sum(num1,num2);
 
             TextView t = (TextView)findViewById(R.id.TVresult);
             t.setText("" + result);
 
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
